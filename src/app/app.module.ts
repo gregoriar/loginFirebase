eee938a6f3281c66ac970c205cd4cf4804b98ebd
gregoriar <gregoriar@gmail.com>
2022-08-22 22:52:52 +0200

    Depuracion de componentes no utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
-//import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-//import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistroComponent } from './components/registro/registro.component';
 import { HomeComponent } from './components/home/home.component';
 import { PagNoEncontradaComponent } from './components/pag-no-encontrada/pag-no-encontrada.component';
-import { ChatComponent } from './components/chat/chat.component';
 import { NohomeComponent } from './components/nohome/nohome.component';
 import { RegistroReactComponent } from './components/registro-react/registro-react.component';
 import { LoginReactComponent } from './components/login-react/login-react.component';
@@ -25,11 +18,8 @@ import { LoginReactComponent } from './components/login-react/login-react.compon
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent,
-    RegistroComponent,
     HomeComponent,
     PagNoEncontradaComponent,
-    ChatComponent,
     NohomeComponent,
     RegistroReactComponent,
     LoginReactComponent
